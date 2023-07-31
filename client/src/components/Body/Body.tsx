5736e780ff4e4dcd614ba2051fb57b6a8e3ab6e3
MoisoDavid <wollxi@gmail.com>
2023-07-31 15:01:48 -0500

    Adding react-slick to the card slider

--- client/src/components/Body/Body.tsx
+++ client/src/components/Body/Body.tsx
@@ -1,4 +1,12 @@
+/* REACT */
 import React from 'react';
+import Slider from "react-slick";
+
+/* CSS */
+import s from "./Body.module.css";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 
 
 export default function Body() {
