b92cd334ab37cee651f8a45b72b6ea1651c43f47
MoisoDavid <wollxi@gmail.com>
2023-07-31 15:23:23 -0500

    Clearly I find that is uneccesary to create a reducer for a simple task, srry for that ;)

--- client/src/components/Body/Body.tsx
+++ client/src/components/Body/Body.tsx
@@ -1,5 +1,5 @@
 /* REACT */
-import React from 'react';
+import {useEffect, useState} from 'react';
 import Slider from "react-slick";
 
 /* CSS */
@@ -12,7 +12,11 @@ export default function Body() {
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-      };
+    };
+
+    useEffect(() => {
+
+    }, [])
 
   return (
     <div>
