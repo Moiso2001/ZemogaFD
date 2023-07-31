2b79d6b5d91d81bba6d3bfcf25d7e751816a5dcc
MoisoDavid <wollxi@gmail.com>
2023-07-31 15:05:20 -0500

    Creating and Adding the card to body

--- client/src/components/Body/Body.tsx
+++ client/src/components/Body/Body.tsx
@@ -7,17 +7,21 @@ import s from "./Body.module.css";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-
-
 export default function Body() {
+    const settings = {
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      };
+
   return (
     <div>
         <div>
             <span>Previous Rulings</span>
         </div>
-        <div>
+        <Slider {...settings}> 
 
-        </div>
+        </Slider>
     </div>
   )
 }
