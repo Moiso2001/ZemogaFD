9d9713da6d585692917b3d0b78b55e9707ead4b1
MoisoDavid <wollxi@gmail.com>
2023-08-01 08:38:29 -0500

    Adding options to change between grid and list

--- client/src/components/Body/Body.tsx
+++ client/src/components/Body/Body.tsx
@@ -2,6 +2,8 @@
 import {useEffect, useState} from 'react';
 import Slider from "react-slick";
 
+import {BiSolidDownArrow} from "react-icons/bi"
+
 /* CSS */
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
@@ -21,6 +23,9 @@ export default function Body() {
     const [view, setView] = useState<string | undefined>()
     const [windowWidth, setWindowWidth] = useState(window.innerWidth)
 
+    const [optionView, setOptionView] = useState<string>('List')
+    const [showOptions, setShowOptions] = useState(false);
+ 
     /* Cards manage states */
     const [cards, setCards] = useState<TheCard[]>()
     const [countOfVotes, setCountOfVotes] = useState<number>(0)
@@ -73,6 +78,7 @@ export default function Body() {
             .catch(e => console.log(e))
     }, [countOfVotes])
 
+
     /* Main function to send the vote, brings the id and kind of vote to put route through controller */
     async function handleSendVote(id: string, vote: string) {
        const successfullySent = await addVote(id, vote)
@@ -88,6 +94,28 @@ export default function Body() {
     <div>
         <div className={s.div_head}>
             <span>Previous Rulings</span>
+
+            {/* Select options to change the view between list and grid */}
+            {
+                view === 'tablet' ||  view === 'desktop'
+                ?<div onClick={_ => setShowOptions(s => !s)}>
+                    <div className={s.custom_select}>
+                       {optionView}
+                       <BiSolidDownArrow className={s.arrow_icon}/>
+
+                       {
+                           showOptions
+                           &&<div className={s.custom_options}>
+                               <div onClick={() => setOptionView('List')} className={s.option_list}>List</div>
+                               <div onClick={() => setOptionView('Grid')} className={s.option_grid}>Grid</div>
+                             </div>     
+                       }
+
+                    </div>
+                 </div>
+                : <></>
+            }
+
         </div>
 
         {
@@ -110,7 +138,22 @@ export default function Body() {
                 </Slider>
             </div>
             :
-            <div></div>
+            <div className={s.cards__list}>
+                {cards?.map(e => 
+                    <Card 
+                        key={e._id}
+                        id={e._id}
+                        name={e.name}
+                        description={e.description}
+                        category={e.category}
+                        picture={e.picture}
+                        lastUpdated={e.lastUpdated}
+                        votes={e.votes}
+                        sendVote={handleSendVote}
+                    />
+                )}
+            </div>
+            
         }
 
     </div>
